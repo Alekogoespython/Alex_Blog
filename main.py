38acc254ac25cb4f12f30751b44c1a29797eb528
= <alexander.kortenbreer@web.de>
2022-08-11 14:27:45 +0100

    removed getting environment variables v2

--- main.py
+++ main.py
@@ -3,7 +3,6 @@
 
 from functools import wraps
 
-import dotenv
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
@@ -21,7 +20,7 @@ from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from flask_gravatar import Gravatar
 from flask_ckeditor import CKEditor, CKEditorField
-import os
+# import os
 # from dotenv import dotenv_values, load_dotenv
 
 # path = dotenv.find_dotenv("sensitive.env")
